be39d4af3ec88dcf93aedb5906f3322764a54181
Muhammad Faisal Rehman <faisalrehman.contact@gmail.com>
2024-04-01 21:24:26 +0500

    redux-toolkit

---
+++ reduxtoolkitapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+function App() {
+  return (
+    <div className='App'>
+      <AddTodo/>
+      <Todos/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ reduxtoolkitapp/src/components/Todos.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+function Todos() {
+  const todos = useSelector((state) => state.todos);
+  //console.log(todos)
+
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div>Todos</div>
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            key={todo.id}
+          >
+            <div className="text-white">{todo.text}</div>
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
+              </svg>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Todos;
